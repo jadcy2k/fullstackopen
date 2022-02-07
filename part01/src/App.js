9382a395e2dc79a5b8e2fdddd144bb602882bfac
Diaz,Alf <alf.diaz@here.com>
2022-02-07 11:29:57 +0100

    First commit, part01

---
+++ part01/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+// SIEMPRE en mayúsculas para evitar coincidencias con etiquetas HTML. 
+const Title = (props) => {
+  // Desestructurar objeto:
+  const {course} = props;
+  // es lo mismo que: const course = props.course;
+  return (
+    <h1>{course}</h1>
+  );
+}
+ // SIEMPRE en mayúsculas para evitar coincidencias con etiquetas HTML.
+const Mensaje = (props) => {
+  // Se puede también devolver la propiedad del objeto "props":
+  return <h1 style={{color: props.color}}>{props.text}</h1>
+}
+
+// Más simple todavía:
+// Componente "One-liner": No necesita el "return" y 
+// se puede acceder a la propiedad con {} y sin pasar el "props":
+const MensajeOneLiner = ({texto}) => <h2>{texto}</h2> 
+ 
+
+const App = () => {
+  return (
+    <div className="App">
+      <Title course="Bootcamp Full Stack 2021" />
+
+      <Mensaje color="red" text="hola qué tal mundo!!" />
+      <Mensaje color="turquoise" text="Otro mensaje." />
+      <Mensaje color="gray" text="Un bonito mensaje." />
+
+      <MensajeOneLiner texto="Esto es un componente 'one-liner'" />
+    </div>
+  );
+}
+
+export default App;
